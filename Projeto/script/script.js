1a83e301c8760a9bf97e1e100afefad777a830af
MarcosGabriel@BK <marcosgabriel20061216@gmail.com>
2023-10-30 01:19:21 -0300

    Finalização da Página Principal
    Acabei de finalizar a página principal

--- Projeto/script/script.js
+++ Projeto/script/script.js
@@ -25,85 +25,65 @@ const slideImage = () => {
   )
 }
 
+// const imgs = document.getElementById("img")
+// const img = document.querySelectorAll("#img img")
 
-// var container = document.getElementById('container')
-// var slider = document.getElementById('slider');
-// var slides = document.getElementsByClassName('slide').length;
-// var buttons = document.getElementsByClassName('btn');
+// let idx = 0;
 
+// function carrossel() {
+//   idx++;
 
-// var currentPosition = 0;
-// var currentMargin = 0;
-// var slidesPerPage = 0;
-// var slidesCount = slides - slidesPerPage;
-// var containerWidth = container.offsetWidth;
-// var prevKeyActive = false;
-// var nextKeyActive = true;
+//   if(idx > img.length - 1) {
+//     idx = 0;
+//   }
 
-// window.addEventListener("resize", checkWidth);
-
-// function checkWidth() {
-//     containerWidth = container.offsetWidth;
-//     setParams(containerWidth);
+//   imgs.style.transform = `translateX(${-idx * 500}px)`;
 // }
 
-// function setParams(w) {
-//     if (w < 551) {
-//         slidesPerPage = 1;
-//     } else {
-//         if (w < 901) {
-//             slidesPerPage = 2;
-//         } else {
-//             if (w < 1101) {
-//                 slidesPerPage = 3;
-//             } else {
-//                 slidesPerPage = 4;
-//             }
-//         }
-//     }
-//     slidesCount = slides - slidesPerPage;
-//     if (currentPosition > slidesCount) {
-//         currentPosition -= slidesPerPage;
-//     };
-//     currentMargin = - currentPosition * (100 / slidesPerPage);
-//     slider.style.marginLeft = currentMargin + '%';
-//     if (currentPosition > 0) {
-//         buttons[0].classList.remove('inactive');
-//     }
-//     if (currentPosition < slidesCount) {
-//         buttons[1].classList.remove('inactive');
-//     }
-//     if (currentPosition >= slidesCount) {
-//         buttons[1].classList.add('inactive');
-//     }
-// }
+// setInterval(carrossel, 1800)
 
-// setParams();
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".wrapper i");
 
-// function slideRight() {
-//     if (currentPosition != 0) {
-//         slider.style.marginLeft = currentMargin + (100 / slidesPerPage) + '%';
-//         currentMargin += (100 / slidesPerPage);
-//         currentPosition--;
-//     };
-//     if (currentPosition === 0) {
-//         buttons[0].classList.add('inactive');
-//     }
-//     if (currentPosition < slidesCount) {
-//         buttons[1].classList.remove('inactive');
-//     }
-// };
 
-// function slideLeft() {
-//     if (currentPosition != slidesCount) {
-//         slider.style.marginLeft = currentMargin - (100 / slidesPerPage) + '%';
-//         currentMargin -= (100 / slidesPerPage);
-//         currentPosition++;
-//     };
-//     if (currentPosition == slidesCount) {
-//         buttons[1].classList.add('inactive');
-//     }
-//     if (currentPosition > 0) {
-//         buttons[0].classList.remove('inactive');
-//     }
-// };
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const showHideIcons = () => {
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+  arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+arrowIcons.forEach(icon => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 15;
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHideIcons(), 60);
+  })
+})
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX;
+  prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+  if(!isDragStart) return;
+  e.preventDefault();
+  carousel.classList.add("dragging");
+  let positionDiff = e.pageX - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+  showHideIcons();
+}
+
+const dragStop = () => {
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
